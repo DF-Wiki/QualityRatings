b1c18d62e8a058f6116e7e7026dab2684edc2f7c
lethosor <lethosor@gmail.com>
2014-04-17 17:37:34 -0400

    Squashed 'QualityRatings/DFWikiFunctions/' changes from 7f5d833..d96bc0b
    d96bc0b Disable DFDiagram for now
    
    git-subtree-dir: QualityRatings/DFWikiFunctions
    git-subtree-split: d96bc0bc4249eca2036c63733ae31a4d374afc6f

--- DFWikiFunctions.php
+++ DFWikiFunctions.php
@@ -2,7 +2,7 @@
 
 require_once 'Settings.php';
 if (!defined('TESTWIKI')) {
-	require_once 'extensions/DFDiagram/DFDiagram.php';
+	#require_once 'extensions/DFDiagram/DFDiagram.php';
 }
 require_once 'tweaks/CVRedirect.php';
 
