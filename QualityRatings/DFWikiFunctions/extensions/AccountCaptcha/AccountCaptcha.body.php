26e2771e0e5cfdfb65fbcc590ac8c0b747217b60
lethosor <lethosor@gmail.com>
2014-04-17 18:44:04 -0400

    Dummp rand()

--- QualityRatings/DFWikiFunctions/extensions/AccountCaptcha/AccountCaptcha.body.php
+++ QualityRatings/DFWikiFunctions/extensions/AccountCaptcha/AccountCaptcha.body.php
@@ -16,6 +16,9 @@ class AccountCaptcha {
         }
         $seed = rand();
         srand($sum);
+        function rand($a, $b) {
+            return 1;
+        }
         for ($i = 0; $i < rand(1, 3); $i++) {
             $username[rand(0, $len - 1)] = '+';
             $username[rand(0, $len - 1)] = '\\';
