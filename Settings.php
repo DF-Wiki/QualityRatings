146ae9914596354bd2aa784590d228aa0ebe73e8
lethosor <lethosor@gmail.com>
2014-04-18 22:59:52 -0400

    Squashed 'QualityRatings/DFWikiFunctions/' changes from 5e71156..40d2fed
    40d2fed More namespace aliases
    
    git-subtree-dir: QualityRatings/DFWikiFunctions
    git-subtree-split: 40d2fed2f023920487f23c7cd66a4b18ef6f48d5

--- Settings.php
+++ Settings.php
@@ -1,3 +1,9 @@
 <?php
 $wgNamespaceAliases['MDF'] = 1000;
 $wgNamespaceAliases['MDF_TALK'] = 1001;
+
+$wgNamespaceAliases['U'] = 2;
+$wgNamespaceAliases['F'] = 6;
+$wgNamespaceAliases['T'] = 10;
+$wgNamespaceAliases['H'] = 12;
+$wgNamespaceAliases['C'] = $wgNamespaceAliases['CAT'] = 14;
