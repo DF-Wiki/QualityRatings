96e469870edea3c33592ec65ce8f6935103d58c9
lethosor <lethosor@gmail.com>
2014-04-18 23:04:05 -0400

    Squashed 'QualityRatings/DFWikiFunctions/' changes from 40d2fed..ddc2d87
    ddc2d87 Version namespace aliases
    
    git-subtree-dir: QualityRatings/DFWikiFunctions
    git-subtree-split: ddc2d870ce2d6c506d49df0ac2836931e83f6563

--- Settings.php
+++ Settings.php
@@ -7,3 +7,15 @@ $wgNamespaceAliases['F'] = 6;
 $wgNamespaceAliases['T'] = 10;
 $wgNamespaceAliases['H'] = 12;
 $wgNamespaceAliases['C'] = $wgNamespaceAliases['CAT'] = 14;
+
+$DFReleases = array(
+    1 => 110,
+    2 => 106,
+    3 => 112,
+    4 => 114,
+);
+
+foreach ($DFReleases as $id => $ns) {
+    $wgNamespaceAliases['Rel' . $id] = $wgNamespaceAliases['V' . $id] = $ns;
+    $wgNamespaceAliases['Rel' . $id . '_talk'] = $wgNamespaceAliases['V' . $id . '_talk'] = $ns;
+}
