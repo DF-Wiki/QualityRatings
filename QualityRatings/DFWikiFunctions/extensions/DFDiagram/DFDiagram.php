aba4b6c416faa7bbb6faf075a6645269551dd2ea
lethosor <lethosor@gmail.com>
2014-04-20 13:42:03 -0400

    debug4

--- QualityRatings/DFWikiFunctions/extensions/DFDiagram/DFDiagram.php
+++ QualityRatings/DFWikiFunctions/extensions/DFDiagram/DFDiagram.php
@@ -6,14 +6,14 @@
 // * This is the file that should be included by LocalSettings.php
 // */
 //
-//if (!isset($wgDFDConfigFile)) {
-//	$wgDFDConfigFile = "$IP/extensions/DFDiagram/diagram_config.txt";
-//}
-//if (!isset($wgDFDDefaultDiagramPath)) {
-//	$wgDFDDefaultDiagramPath = "$IP/extensions/DFDiagram/default_diagram.txt";
-//}
-//
-//require_once 'Diagram.php';
+if (!isset($wgDFDConfigFile)) {
+	$wgDFDConfigFile = "$IP/extensions/DFDiagram/diagram_config.txt";
+}
+if (!isset($wgDFDDefaultDiagramPath)) {
+	$wgDFDDefaultDiagramPath = "$IP/extensions/DFDiagram/default_diagram.txt";
+}
+
+require_once 'Diagram.php';
 //$wgExtensionMessagesFiles['DFDiagram'] = dirname( __FILE__ ) . '/DFDiagram.i18n.php';
 //
 //#$DFDFile = new DFDBlockFile($wgDFDConfigFile);
@@ -54,6 +54,6 @@ $wgExtensionCredits['DFDiagram'][] = array(
 	'author' =>'Lethosor',
 	'url' => 'https://github.com/lethosor/DFDiagram',
 	'description' => 'Adds support for DF-style diagrams',
-	'version'  => '0.4-debug3',
+	'version'  => '0.4-debug4',
 );
 
