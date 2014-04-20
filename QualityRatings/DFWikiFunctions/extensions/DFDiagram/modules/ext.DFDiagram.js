17b7bec9958e381d469ffc463a12b7f08980c190
Lethosor <lethosor@users.noreply.github.com>
2014-04-20 16:02:49 -0400

    Update tileset URL

--- QualityRatings/DFWikiFunctions/extensions/DFDiagram/modules/ext.DFDiagram.js
+++ QualityRatings/DFWikiFunctions/extensions/DFDiagram/modules/ext.DFDiagram.js
@@ -1,5 +1,5 @@
 jQuery(function($){
-	Tileset.Font.loadFromURL(wgScriptPath + '/extensions/QualityRatings/DFWikiFunctions/extensions/DFDiagram/resources/8x12.png').on('ready', function(evt, font){
+	Tileset.Font.loadFromURL('http://dwarffortresswiki.org/images/b/b7/Default_tileset_8x12.png').on('ready', function(evt, font){
 		$('.dfdiagram').each(function(i, e){
 			var frameList = $();
 			$(e).find('.dfdiagram-frame').each(function(i, e) {
