de9384e8d6166509ea6c2e0f43bbd2479386614e
lethosor <lethosor@gmail.com>
2014-05-13 19:53:34 -0400

    Retrieve log and return most recent rating

--- QualityRatings.body.php
+++ QualityRatings.body.php
@@ -11,6 +11,9 @@ $wgQualityRatings = array(
 
 class QualityRatingHandler {
 	private static $ratingCache = array();
+	private static $defaultLogEntry = array(
+		'rating' => -1,
+	);
 	public static function toTitle ($title) {
 		if (!($title instanceof Title)) {
 			$title = Title::newFromText((string)$title);
@@ -20,13 +23,26 @@ class QualityRatingHandler {
 	public static function getRatingData ($title) {
 		$title = self::toTitle($title);
 		$title = Title::newFromText($title->getPrefixedDBkey() . '/rating_log');
-		print($title->getPrefixedDBkey());
+		$content = WikiPage::factory($title)->getText();
+		$data = json_decode($content, true);   // Return objects as arrays
+		if (!is_array($data)) {
+			// Log does not exist
+			return array(self::$defaultLogEntry);
+		}
+		foreach ($data as $k => $d) {
+			if (!is_array($d)) {
+				$data[$k] = self::$defaultLogEntry;
+			}
+		}
+		return $data;
 	}
 	public static function getRating ($title) {
 		$title = self::toTitle($title);
 		$dbkey = $title->getPrefixedDBkey();
 		if (!array_key_exists($dbkey, self::$ratingCache)) {
-			self::$ratingCache[$dbkey] = 1;
+			$data = self::getRatingData($title);
+			$data = end($data);
+			self::$ratingCache[$dbkey] = $data['rating'];
 		}
 		return self::$ratingCache[$dbkey];
 	}
