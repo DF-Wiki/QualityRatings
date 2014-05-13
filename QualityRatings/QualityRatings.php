300dc26e274bc980b02f177784ac9b537f29eccb
lethosor <lethosor@gmail.com>
2014-05-13 17:25:23 -0400

    Rename QualityRatings.body.php to QualityRatings.funcs.php

--- QualityRatings/QualityRatings.php
+++ QualityRatings/QualityRatings.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'QualityRatings.body.php';
+require_once 'QualityRatings.funcs.php';
 if (!defined('TESTWIKI')) {
     require_once 'DFWikiFunctions/DFWikiFunctions.php';
 }
@@ -33,6 +33,6 @@ $wgExtensionCredits['QualityRatings'][] = array(
 	'author' =>'[[User:Lethosor|Lethosor]]',
 	'url' => 'https://github.com/lethosor/dfwiki-rater',
 	'description' => 'Dwarf Fortress wiki rating script',
-	'version'  => '1.0.5-dev',
+	'version'  => '2.0-alpha',
 );
 
