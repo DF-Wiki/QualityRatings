14f9b3faaf2efb2b707b5ee51e52634fa0eb3e7e
lethosor <lethosor@gmail.com>
2019-06-09 23:55:02 -0400

    Reorganize

--- QualityRatings.php
+++ QualityRatings.php
@@ -4,7 +4,7 @@ require_once 'QualityRatings.body.php';
 
 $wgResourceModules['ext.QualityRatings'] = array(
 	'scripts' => array(
-		'../rater.js',
+		'modules/rater.js',
 		'modules/jquery.color-2.1.0.min.js'
 	),
 	'localBasePath' => __DIR__,
@@ -28,8 +28,8 @@ $wgExtensionCredits['QualityRatings'][] = array(
 	'path' => __FILE__,
 	'name' => 'QualityRatings',
 	'author' =>'[[User:Lethosor|Lethosor]]',
-	'url' => 'https://github.com/lethosor/dfwiki-rater',
+	'url' => 'https://github.com/DF-Wiki/dfwiki-rater',
 	'description' => 'Dwarf Fortress wiki rating script',
-	'version'  => '1.0.5-dev',
+	'version'  => '1.0.6',
 );
 
